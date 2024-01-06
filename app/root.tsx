9023dbe90e4ccd03f3d50cafb64710323e749eec
Jack McNicol <jackmcpickle@gmail.com>
2024-01-06 15:55:51 +1030

    update with cloudflare config

--- app/root.tsx
+++ app/root.tsx
@@ -1,40 +1,37 @@
-import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
-import stylesheet from "~/tailwind.css";
+import type { LinksFunction } from '@remix-run/cloudflare';
 import {
-  Link,
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-  useRevalidator,
-} from "@remix-run/react";
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+} from '@remix-run/react';
+import { cssBundleHref } from '@remix-run/css-bundle';
 
-
-//Tailwind css links - ensure the tailwind.css file is included in the app folder
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
+    ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
 ];
 
 // App export from root
 export default function App() {
-  
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    );
 }
